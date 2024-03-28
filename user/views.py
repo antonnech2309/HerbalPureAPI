718f2ca6d87a4067419bae0ddec94c623245c2d3
Anton <anton380969658858@gmail.com>
2024-03-28 18:54:08 +0200

    add user credentials returning when loggining

--- user/views.py
+++ user/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
 from rest_framework import generics
-from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework.response import Response
 
 from user.serializers import UserSerializer
 
@@ -18,3 +20,27 @@ class ManageUserView(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        return token
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        email = request.data.get("email")
+        user = get_user_model().objects.get(email=email)
+        user_info_serializer = UserSerializer(user)
+        user_info_data = user_info_serializer.data
+        data = {
+            "access": response.data["access"],
+            "refresh": response.data["refresh"],
+            "user": user_info_data
+        }
+        return Response(data)
