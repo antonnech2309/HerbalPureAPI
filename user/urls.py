b55b4a489b1660dd9cf710d912adf0a2820d6622
Anton <anton380969658858@gmail.com>
2024-03-29 20:53:12 +0200

    add user credentials to refresh token endpoint

--- user/urls.py
+++ user/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from user.views import CreateUserView, ManageUserView, CustomTokenObtainPairView
+from user.views import (
+    CreateUserView,
+    ManageUserView,
+    CustomTokenObtainPairView,
+    CustomTokenRefreshView
+)
 
 app_name = "user"
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
     path("token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/refresh/", CustomTokenRefreshView.as_view(), name="token_refresh"),
     path("me/", ManageUserView.as_view(), name="manage"),
 ]
