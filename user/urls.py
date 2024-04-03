2edbae0f1437a8e53c3e52ad03db56fffc1b612b
Anton <anton380969658858@gmail.com>
2024-04-03 21:44:52 +0300

    add endpoint contact us

--- user/urls.py
+++ user/urls.py
@@ -4,14 +4,23 @@ from user.views import (
     CreateUserView,
     ManageUserView,
     CustomTokenObtainPairView,
-    CustomTokenRefreshView
+    CustomTokenRefreshView, ContactUsView
 )
 
 app_name = "user"
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
-    path("token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh/", CustomTokenRefreshView.as_view(), name="token_refresh"),
+    path(
+        "token/",
+        CustomTokenObtainPairView.as_view(),
+        name="token_obtain_pair"
+    ),
+    path(
+        "token/refresh/",
+        CustomTokenRefreshView.as_view(),
+        name="token_refresh"
+    ),
     path("me/", ManageUserView.as_view(), name="manage"),
+    path("contact-us/", ContactUsView.as_view(), name="contact_us")
 ]
--- user/views.py
+++ user/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.core.mail import send_mail
 from jwt import InvalidTokenError
 from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
@@ -9,7 +10,9 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from rest_framework.response import Response
 
-from user.serializers import UserSerializer, CustomTokenObtainPairSerializer, CustomTokenRefreshSerializer
+from HerbalPureAPI import settings
+from user.serializers import UserSerializer, CustomTokenObtainPairSerializer, CustomTokenRefreshSerializer, \
+    ContactUsSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
@@ -59,3 +62,33 @@ class CustomTokenRefreshView(TokenRefreshView):
             return Response(data)
         except (InvalidTokenError, TokenError):
             return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+
+class ContactUsView(generics.CreateAPIView):
+    serializer_class = ContactUsSerializer
+    permission_classes = []
+    authentication_classes = []
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data["name"]
+            email = serializer.validated_data["email"]
+            message = serializer.validated_data["message"]
+
+            send_mail(
+                f"Message from {name} ({email})",
+                message,
+                settings.EMAIL_HOST_USER,
+                [settings.EMAIL_HOST_USER],
+            )
+
+            return Response(
+                {"message": "Message sent successfully!"},
+                status=status.HTTP_200_OK
+            )
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
